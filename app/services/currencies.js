a57b946b96edfd001496da92f386f8ca01dfcd63
Bernard Ojengwa <bernardojengwa@gmail.com>
2024-01-06 21:48:23 +0100

    fix currencies

--- app/services/currencies.js
+++ app/services/currencies.js
@@ -1,5 +1,151 @@
 import ccxt from 'ccxt'
 
+const safaricoin = {
+  id: 'SFX',
+  name: 'Safaricoin',
+  code: 'SFX',
+  precision: 8,
+  info: {
+    coin: 'SFX',
+    depositAllEnable: true,
+    withdrawAllEnable: true,
+    name: 'Safaricoin',
+    free: '0',
+    locked: '0',
+    freeze: '0',
+    withdrawing: '0',
+    ipoing: '0',
+    ipoable: '0',
+    storage: '0',
+    isLegalMoney: false,
+    trading: true,
+    networkList: [
+      {
+        network: 'BSC',
+        coin: 'ETH',
+        withdrawIntegerMultiple: '0.00000001',
+        isDefault: false,
+        depositEnable: true,
+        withdrawEnable: true,
+        depositDesc: '',
+        withdrawDesc: '',
+        specialTips: '',
+        specialWithdrawTips:
+          'The network you have selected is BSC. Please ensure that the withdrawal address supports the BNB Smart Chain network. You will potentially lose your assets if the chosen platform does not support refunds of wrongfully deposited assets.',
+        name: 'BNB Smart Chain (BEP20)',
+        resetAddressStatus: false,
+        addressRegex: '^(0x)[0-9A-Fa-f]{40}$',
+        memoRegex: '',
+        withdrawFee: '0.00017',
+        withdrawMin: '0.00034',
+        withdrawMax: '10000000000',
+        minConfirm: '15',
+        unLockConfirm: '0',
+        sameAddress: false,
+        estimatedArrivalTime: '3',
+        busy: false,
+        contractAddressUrl: 'https://bscscan.com/token/',
+        contractAddress: '0x2170ed0880ac9a755fd29b2688956bd959f933f8'
+      },
+      {
+        network: 'ETH',
+        coin: 'ETH',
+        withdrawIntegerMultiple: '0.00000001',
+        isDefault: true,
+        depositEnable: true,
+        withdrawEnable: true,
+        depositDesc: '',
+        withdrawDesc: '',
+        specialTips:
+          'DO NOT define your Binance deposit address as the receiving address for validator rewards. Validator rewards sent from the node to Binance deposit address is not supported and will not be credited. This will lead to asset loss.',
+        specialWithdrawTips: '',
+        name: 'Ethereum (ERC20)',
+        resetAddressStatus: false,
+        addressRegex: '^(0x)[0-9A-Fa-f]{40}$',
+        memoRegex: '',
+        withdrawFee: '0.0032',
+        withdrawMin: '0.01',
+        withdrawMax: '10000000000',
+        minConfirm: '6',
+        unLockConfirm: '64',
+        sameAddress: false,
+        estimatedArrivalTime: '4',
+        busy: false,
+        contractAddressUrl: 'https://etherscan.io/token/'
+      }
+    ]
+  },
+  active: true,
+  deposit: true,
+  withdraw: true,
+  networks: [
+    {
+      network: 'BSC',
+      coin: 'ETH',
+      withdrawIntegerMultiple: '0.00000001',
+      isDefault: false,
+      depositEnable: true,
+      withdrawEnable: true,
+      depositDesc: '',
+      withdrawDesc: '',
+      specialTips: '',
+      specialWithdrawTips:
+        'The network you have selected is BSC. Please ensure that the withdrawal address supports the BNB Smart Chain network. You will potentially lose your assets if the chosen platform does not support refunds of wrongfully deposited assets.',
+      name: 'BNB Smart Chain (BEP20)',
+      resetAddressStatus: false,
+      addressRegex: '^(0x)[0-9A-Fa-f]{40}$',
+      memoRegex: '',
+      withdrawFee: '0.00017',
+      withdrawMin: '0.00034',
+      withdrawMax: '10000000000',
+      minConfirm: '15',
+      unLockConfirm: '0',
+      sameAddress: false,
+      estimatedArrivalTime: '3',
+      busy: false,
+      contractAddressUrl: 'https://bscscan.com/token/',
+      contractAddress: '0x2170ed0880ac9a755fd29b2688956bd959f933f8'
+    },
+    {
+      network: 'ETH',
+      coin: 'ETH',
+      withdrawIntegerMultiple: '0.00000001',
+      isDefault: true,
+      depositEnable: true,
+      withdrawEnable: true,
+      depositDesc: '',
+      withdrawDesc: '',
+      specialTips:
+        'DO NOT define your Binance deposit address as the receiving address for validator rewards. Validator rewards sent from the node to Binance deposit address is not supported and will not be credited. This will lead to asset loss.',
+      specialWithdrawTips: '',
+      name: 'Ethereum (ERC20)',
+      resetAddressStatus: false,
+      addressRegex: '^(0x)[0-9A-Fa-f]{40}$',
+      memoRegex: '',
+      withdrawFee: '0.0032',
+      withdrawMin: '0.01',
+      withdrawMax: '10000000000',
+      minConfirm: '6',
+      unLockConfirm: '64',
+      sameAddress: false,
+      estimatedArrivalTime: '4',
+      busy: false,
+      contractAddressUrl: 'https://etherscan.io/token/'
+    }
+  ],
+  fee: 0.0032,
+  fees: {
+    BSC: 0.00017,
+    ETH: 0.0032
+  },
+  limits: {
+    leverage: {},
+    amount: {},
+    price: {},
+    cost: {}
+  }
+}
+
 export default (fastify, opts, next) => {
   fastify.get(
     '/currencies',
@@ -26,9 +172,9 @@ export default (fastify, opts, next) => {
       const _currencies = Object.values(all).filter(
         (currency) => !currency.info.isLegalMoney
       )
-      // const currencies = [safaricoin, ..._currencies]
+      const currencies = [safaricoin, ..._currencies]
 
-      return _currencies
+      return currencies
     }
   )
 
